4fb5e7f00a972d7f2a3ff9ab6a5fb3dbd74e5daf
Tai DV <tai@wondaris.com>
2024-04-04 12:48:58 +0700

    fix: create env for development

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -28,12 +28,5 @@ module Backend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # config/application.rb
-    env_file = File.join(Rails.root, 'config', 'local_env.yml')
-
-    YAML.load_file(env_file).each do |key, value|
-      ENV[key.to_s] = value
-    end if File.exist?(env_file)
   end
 end
